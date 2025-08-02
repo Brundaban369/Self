031c1aad46181f8490dce05de872db55049cf5a9
Brundaban Rohidas <rohidasbrundaban6@gmail.com>
2025-08-02 23:06:19 +0530

    Added some library and repositories

--- R_Programming/first.R
+++ R_Programming/first.R
@@ -1,12 +1,27 @@
 # Arithmetic operation ----
-3+7 #add 3+7
-#we will add x in y 
+3 + 7 #add 3+7
+#we will add x in y
 x <- 19
 y <- 26
-x+y
-x*y
-x/y
+x + y
+x * y
+x / y
 x^y
 
 
 #Some other operations ----
+df <- c(1, 2, 3, 4, 5, 6)
+df
+
+df <- seq(1, 10, by = 2) #sequence from 1 to 10 with step of 2
+df
+
+#package install ----
+install.packages("readxl")
+library(readxl)
+install.packages("tidyverse")
+library(tidyverse)
+
+
+# set Repositories ()----
+setRepositories()
